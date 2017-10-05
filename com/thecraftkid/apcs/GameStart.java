51e5c39e7cd137cec10112fd25aca1fd7d509366
Willie Chalmers III <willieechalmers@gmail.com>
2017-10-05 10:08:40 -0500

    Work on GameStart

--- com/thecraftkid/apcs/GameStart.java
+++ com/thecraftkid/apcs/GameStart.java
@@ -10,38 +10,13 @@ import java.awt.*;
  */
 public class GameStart extends Panel {
 
+    private static final boolean RESIZEABLE = false;
+
     private Image offScreenImage;
     private Dimension offScreenSize;
     private Graphics offScreenGraphics;
-    private static final boolean RESIZEABLE = false;
-    int width = 1280 - 10;
-    int height = 1024 - 10;
-    Image virtualMem;
-    Graphics2D gBuffer;
-
-    public void resizeWindow() {
-        if (RESIZEABLE) {
-            if (getHeight() != height + 10 || getWidth() != width + 10) {
-                height = getHeight() - 10;
-                width = getWidth() - 10;
-                virtualMem = createImage(width + 20, height + 20);
-                gBuffer = (Graphics2D) virtualMem.getGraphics();
-                gBuffer.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-            }
-        } else setSize(width + 10, height + 10);
-    }
-
-    public void update(Graphics g) {
-        Dimension d = size();
-        if ((offScreenImage == null) || (d.width != offScreenSize.width) || (d.height != offScreenSize.height)) {
-            offScreenImage = createImage(d.width, d.height);
-            offScreenSize = d;
-            offScreenGraphics = offScreenImage.getGraphics();
-        }
-        offScreenGraphics.clearRect(0, 0, d.width, d.height);
-        paint(offScreenGraphics);
-        g.drawImage(offScreenImage, 0, 0, null);
-    }
+    private int width = 1280 - 10;
+    private int height = 1024 - 10;
 
     public static void main(String[] args) {
         Frame f = new Frame();
@@ -57,6 +32,37 @@ public class GameStart extends Panel {
         window.init();
         f.setSize(1050, 700); // Size of frame
         f.setVisible(true);
+    }
+
+    public void resizeWindow() {
+        if (RESIZEABLE) {
+            if (getHeight() != height + 10 || getWidth() != width + 10) {
+                height = getHeight() - 10;
+                width = getWidth() - 10;
+                Image virtualMem = createImage(width + 20, height + 20);
+                Graphics2D gBuffer = (Graphics2D) virtualMem.getGraphics();
+                gBuffer.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+            }
+        } else {
+            setSize(width + 10, height + 10);
+        }
+    }
+
+    /**
+     * Constantly redraws the displayed image
+     *
+     * @param g
+     */
+    public void update(Graphics g) {
+        Dimension d = getSize();
+        if ((offScreenImage == null) || (d.width != offScreenSize.width) || (d.height != offScreenSize.height)) {
+            offScreenImage = createImage(d.width, d.height);
+            offScreenSize = d;
+            offScreenGraphics = offScreenImage.getGraphics();
+        }
+        offScreenGraphics.clearRect(0, 0, d.width, d.height);
+        paint(offScreenGraphics);
+        g.drawImage(offScreenImage, 0, 0, null);
     }
 
     /**
