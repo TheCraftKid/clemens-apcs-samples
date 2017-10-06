09a8f20aec955b1dd98eb3d74989691257f7445e
Willie Chalmers III <willieechalmers@gmail.com>
2017-10-05 23:00:32 -0500

    Add documentation, add variable for largest amount of funds amassed, and other minor additions

--- com/thecraftkid/apcs/chalmersw08.java
+++ com/thecraftkid/apcs/chalmersw08.java
@@ -17,7 +17,7 @@ public class chalmersw08 {
     }
 
     /**
-     * Returns the user-inputted
+     * Returns the user-inputted amount of money to start the game.
      */
     private static int requestStartingFunds() {
         Scanner scanner = new Scanner(System.in);
@@ -26,7 +26,7 @@ public class chalmersw08 {
     }
 
     /**
-     * The implementation of a die game that follows these rules:
+     * A die game that follows these rules:
      * <ul>
      * <li>If the sum of two die rolls equals 7, win $4</li>
      * <li>If the sum of two die rolls does not equal 7, lose $1</li>
@@ -35,7 +35,7 @@ public class chalmersw08 {
      */
     public static class DieGame {
 
-        private static final int FUND_INCREMENT = 7;
+        private static final int FUND_INCREMENT = 4;
 
         private static final int FUND_DECREMENT = 1;
 
@@ -45,12 +45,20 @@ public class chalmersw08 {
 
         private int rolls;
 
+        private int largestFundsValue;
+
         private DieGame(int startingFunds) {
-            die = new Die();
+            die = new StandardDie();
             funds = startingFunds;
+            largestFundsValue = funds;
             rolls = 0;
         }
 
+        /**
+         * Starts a new instance of this game.
+         *
+         * @param startingFunds The amount of money to begin the game
+         */
         public static void startNewGame(int startingFunds) {
             DieGame game = new DieGame(startingFunds);
             while (game.funds >= 0) {
@@ -63,12 +71,19 @@ public class chalmersw08 {
 
         private void displayGameOver() {
             System.out.println("Rolls: " + rolls + " Funds: " + funds);
+            System.out.println(
+                    String.format("You should have stopped at %s rolls when you had $%s.",
+                    largestFundsValue, 0)); // FIXME: 10/5/17
         }
 
+        /**
+         * Returns this game's die
+         *
+         * @return Outputted value from {@link Die#roll()}
+         */
         public int getNextRoll() {
             return die.roll();
         }
-
 
         public void onRoll(int first, int second) {
             System.out.println(first + " " + second);
@@ -78,6 +93,7 @@ public class chalmersw08 {
                 subtractFunds(FUND_DECREMENT);
             }
             rolls++;
+            System.out.println("Funds " + funds);
         }
 
         public void addFunds(int amount) {
@@ -89,16 +105,50 @@ public class chalmersw08 {
         }
     }
 
-    public static class Die {
+    /**
+     * A {@link Die} that only rolls numbers between 1 and 6, both inclusive.
+     */
+    public static class StandardDie extends Die {
 
         private static final int MAX_VALUE = 6;
 
         private static final int MIN_VALUE = 1;
 
+        public StandardDie() {
+            super(MAX_VALUE, MIN_VALUE);
+        }
+    }
+
+    /**
+     * An extensible die that generates random numbers.
+     */
+    public static class Die {
+
+        private final int maxValue;
+
+        private final int minValue;
+
         private int lastValue;
 
+        /**
+         * Creates a new die that rolls values from the given maximum to the the given minimum,
+         * inclusive.
+         *
+         * @param maxValue The largest value this die can generate.
+         * @param minValue The smallest value this die can generate.
+         */
+        public Die(int maxValue, int minValue) {
+            this.maxValue = maxValue;
+            this.minValue = minValue;
+        }
+
+        /**
+         * Returns a value between {@link #maxValue} and {@link #minValue}.
+         * </p>
+         * This also stores the returned value as the {@link #lastValue}.
+         */
         public int roll() {
-            lastValue = (int) (Math.random() * (MAX_VALUE - MIN_VALUE + 1) + MIN_VALUE);
+            lastValue = (int) (Math.random() * (maxValue - minValue + 1) + minValue);
             return lastValue;
         }
 
