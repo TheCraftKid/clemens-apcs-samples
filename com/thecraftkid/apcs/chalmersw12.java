0159a7aa9e6d598c1ccdf01c92670504dda2b799
Willie Chalmers III <willieechalmers@gmail.com>
2017-11-06 10:02:10 -0600

    Implement insertion sort and fix clone method

--- com/thecraftkid/apcs/chalmersw12.java
+++ com/thecraftkid/apcs/chalmersw12.java
@@ -5,7 +5,8 @@ import com.thecraftkid.apcs.given.LabHelper;
 import static java.lang.System.out;
 
 /**
- * A program that sorts integers from least to greatest.
+ * A program that sorts integers from least to greatest using different
+ * algorithms including insertion sort and selection sort.
  *
  * @author Willie Chalmers III
  * @since 11/3/17
@@ -15,35 +16,47 @@ public class chalmersw12 {
     public static void main(String[] args) {
         int[] items = LabHelper.lab12();
         int[] insertSortedItems = sortInsertion(items);
-        int[] selectionSortedItems = sortSelection(items);
+//        int[] selectionSortedItems = sortSelection(items);
         printList(insertSortedItems);
-        printList(selectionSortedItems);
+//        printList(selectionSortedItems);
     }
 
+    /**
+     * Performs an insertion sort on the given array.
+     *
+     * @param list An unsorted list
+     * @return A sorted copy of the array
+     */
     public static int[] sortInsertion(int[] list) {
-        int[] sorted = new int[list.length];
-        for (int i = 0; i < list.length; i++) {
-            int toSort = sorted[i];
-            for (int j = 0; j < list.length; j++) {
-                int next = sorted[i];
-
-            }
-        }
-        return sorted;
-    }
-
-    public static int[] sortSelection(int[] list) {
         int[] sorted = clone(list);
-        for (int i = 1; i < list.length + 1; i++) {
-            int first = sorted[i];
-            int next = sorted[i + 1];
-            if (next < first) {
-                sorted[i] = next;
-                sorted[i - 1] = first;
+        for (int i = 0; i < sorted.length; i++) {
+            int j = i;
+            while (j > 0 && sorted[j] < sorted[j - 1]) {
+                swap(sorted, j, j - 1);
+                j -= 1;
             }
         }
         return sorted;
     }
+
+    public static void swap(int[] list, int start, int end) {
+        int toSwap = list[start];
+        list[start] = list[end];
+        list[end] = toSwap;
+    }
+
+//    public static int[] sortSelection(int[] list) {
+//        int[] sorted = clone(list);
+//        for (int i = 1; i < list.length + 1; i++) {
+//            int first = sorted[i];
+//            int next = sorted[i - 1];
+//            if (next < first) {
+//                sorted[i] = next;
+//                sorted[i - 1] = first;
+//            }
+//        }
+//        return sorted;
+//    }
 
     /**
      * Sorts the given list in reverse order
@@ -58,7 +71,7 @@ public class chalmersw12 {
     public static int[] clone(int[] list) {
         int[] result = new int[list.length];
         for (int i = 0; i < result.length; i++) {
-            result[0] = list[0];
+            result[i] = list[i];
         }
         return result;
     }
