93c0cd9d260818ab5ca73b42462e83e62878ac16
Willie Chalmers III <willieechalmers@gmail.com>
2017-11-06 10:28:54 -0600

    Work on Lab 12

--- com/thecraftkid/apcs/chalmersw12.java
+++ com/thecraftkid/apcs/chalmersw12.java
@@ -2,7 +2,7 @@ package com.thecraftkid.apcs;
 
 import com.thecraftkid.apcs.given.LabHelper;
 
-import static java.lang.System.out;
+import java.util.Arrays;
 
 /**
  * A program that sorts integers from least to greatest using different
@@ -15,70 +15,99 @@ public class chalmersw12 {
 
     public static void main(String[] args) {
         int[] items = LabHelper.lab12();
-        int[] insertSortedItems = sortInsertion(items);
-//        int[] selectionSortedItems = sortSelection(items);
+        Sorter sorter = new Sorter(items);
+        System.out.println("Original: \n" + sorter);
+        int[] insertSortedItems = sorter.sortInsertion();
+//        int[] selectionSortedItems = sortSelection(original);
+        System.out.println("Sorted with insertion sort: ");
         printList(insertSortedItems);
 //        printList(selectionSortedItems);
     }
 
-    /**
-     * Performs an insertion sort on the given array.
-     *
-     * @param list An unsorted list
-     * @return A sorted copy of the array
-     */
-    public static int[] sortInsertion(int[] list) {
-        int[] sorted = clone(list);
-        for (int i = 0; i < sorted.length; i++) {
-            int j = i;
-            while (j > 0 && sorted[j] < sorted[j - 1]) {
-                swap(sorted, j, j - 1);
-                j -= 1;
+    static void printList(int[] list) {
+        StringBuilder builder = new StringBuilder();
+        builder.append("[");
+        Arrays.stream(list).forEach(i -> builder.append(i).append(", ")); // Gotta love Java 8
+        builder.append("]");
+        System.out.println(builder);
+    }
+
+    public static class Sorter {
+
+        private int[] original;
+
+        private SortCallback callback;
+
+        public Sorter(int[] items) {
+            this.original = clone(items);
+        }
+
+        public Sorter(int[] items, SortCallback callback) {
+            this.original = items;
+            this.callback = callback;
+        }
+
+        /**
+         * Performs an insertion sort with this sorter's item.
+         *
+         * @return A sorted copy of the array
+         */
+        public int[] sortInsertion() {
+            int[] sorted = clone(original);
+            for (int i = 0; i < sorted.length; i++) {
+                int j = i;
+                while (j > 0 && sorted[j] < sorted[j - 1]) {
+                    swap(sorted, j, j - 1);
+                    j -= 1;
+                }
+
             }
+            return sorted;
         }
-        return sorted;
-    }
 
-    public static void swap(int[] list, int start, int end) {
-        int toSwap = list[start];
-        list[start] = list[end];
-        list[end] = toSwap;
-    }
-
-//    public static int[] sortSelection(int[] list) {
-//        int[] sorted = clone(list);
-//        for (int i = 1; i < list.length + 1; i++) {
-//            int first = sorted[i];
-//            int next = sorted[i - 1];
-//            if (next < first) {
-//                sorted[i] = next;
-//                sorted[i - 1] = first;
-//            }
-//        }
-//        return sorted;
-//    }
-
-    /**
-     * Sorts the given list in reverse order
-     *
-     * @return
-     */
-    public static int[] sortBackwards(int[] list) {
-        int[] sorted = clone(list);
-        return sorted;
-    }
-
-    public static int[] clone(int[] list) {
-        int[] result = new int[list.length];
-        for (int i = 0; i < result.length; i++) {
-            result[i] = list[i];
+        /**
+         * Performs a selection sort with this sorter's items.
+         *
+         * @return A sorted copy of the array
+         */
+        public int[] sortSelection() {
+            int[] sorted = clone(original);
+            for (int i = 1; i < sorted.length + 1; i++) {
+            }
+            return sorted;
         }
-        return result;
-    }
 
-    public static void printList(int[] list) {
-        for (int value : list) {
-            out.println(value);
+        /**
+         * Sorts the given list in reverse order.
+         *
+         * @return The sorted array
+         */
+        public int[] sortBackwards() {
+            int[] sorted = clone(original);
+            return sorted;
+        }
+
+        public static void swap(int[] list, int start, int end) {
+            int toSwap = list[start];
+            list[start] = list[end];
+            list[end] = toSwap;
+        }
+
+        public static int[] clone(int[] list) {
+            int[] result = new int[list.length];
+            for (int i = 0; i < result.length; i++) {
+                result[i] = list[i];
+            }
+            return result;
+        }
+
+        @Override
+        public String toString() {
+            return Arrays.toString(original);
+        }
+
+        public interface SortCallback {
+            void onUpdate(int[] list, int position, int oldValue, int newValue);
         }
     }
 }
