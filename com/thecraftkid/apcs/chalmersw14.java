b8cb4109ecec6858d96e42053dc57e9820fe72ae
Willie Chalmers III <willieechalmers@gmail.com>
2017-11-13 10:20:43 -0600

    Continue work on Lab 14
    - Refactor CardPlayer into Deck class
    - Add documentation for many methods
    - Begin work on shuffling deck
    - Implement toString for Deck
    - Implement compareTo in ChalmersCard

--- com/thecraftkid/apcs/chalmersw14.java
+++ com/thecraftkid/apcs/chalmersw14.java
@@ -1,10 +1,8 @@
 package com.thecraftkid.apcs;
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.function.Predicate;
+import java.util.*;
+import java.util.concurrent.ThreadLocalRandom;
+import java.util.stream.Collectors;
 
 /**
  * @author Willie Chalmers III
@@ -17,43 +15,101 @@ public class chalmersw14 {
     }
 
     public static class CardPlayer {
-        private List<ChalmersCard> cards = new ArrayList<>(52);
 
+        private Deck playerOneDeck = new Deck();
+        private Deck playerTwoDeck = new Deck();
 
-        public void fillDeck() {
-            cards.replaceAll(chalmersCard -> new ChalmersCard());
-        }
-
-        public void deleteCards() {
-            cards.removeIf(card -> true);
-        }
-
-        @Override
-        public String toString() {
-            cards.stream()
-                    .
-        }
-
-        public void displayCards() {
-            System.out.println(this);
-        }
-
-        public void shuffleDeck() {
-
-        }
-
-        public void sortByValue() {
+        public static void startWarGame() {
 
         }
     }
 
-    public static class ChalmersCard implements Comparable {
+    public static class Deck {
+
+        private List<ChalmersCard> cards = new ArrayList<>();
+
+        public void fillDeck() {
+//            cards.replaceAll(chalmersCard -> new ChalmersCard());
+            while (cards.size() <= 52) {
+
+                for (int value : ChalmersCard.POSSIBLE_VALUES) {
+
+                }
+            }
+        }
+
+        /**
+         * Removes all cards from this deck.
+         */
+        public void clearDeck() {
+            cards.removeIf(card -> true);
+        }
+
+        /**
+         * Prints {@link ChalmersCard#toString()} to standard output for each
+         * card in this deck.
+         */
+        public void displayCards() {
+            System.out.println(this);
+        }
+
+        /**
+         * Places each of the cards in this deck into a new random position.
+         */
+        public void shuffleDeck() {
+            List<ChalmersCard> copy = new ArrayList<>();
+            Collections.copy(copy, cards);
+            List<Integer> randoms = generateRandomPositions();
+        }
+
+        private List<Integer> generateRandomPositions() {
+            List<Integer> randoms = new ArrayList<>();
+            while (randoms.size() <= 52) {
+                int nextRandom = ThreadLocalRandom.current().nextInt(0, 52 + 1);
+                if (randoms.get(nextRandom) == -1) {
+                    randoms.add(nextRandom);
+                }
+            }
+            return randoms;
+        }
+
+        /**
+         * Returns a sorted copy of this deck.
+         */
+        public List<ChalmersCard> getSortedDeck() {
+            return cards.stream()
+                    .sorted(Comparator.naturalOrder())
+                    .collect(Collectors.toList());
+        }
+
+        /**
+         * Returns {@link ChalmersCard#toString()} for each in this deck.
+         */
+        @Override
+        public String toString() {
+            return String.valueOf(cards);
+        }
+    }
+
+    public static class ChalmersCard implements Comparable<ChalmersCard> {
+
+        /**
+         * Jack, queen, and king are worth 10.
+         */
         private static final List<Integer> POSSIBLE_VALUES = Collections.unmodifiableList(
                 Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10));
+        /**
+         * Consist of A, 2, 3, 4, 5, 6, 7, 8, 9, 10, J, Q, K
+         */
         private static final List<String> POSSIBLE_FACES = Collections.unmodifiableList(
-                Arrays.asList("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "K", "Q"));
+                Arrays.asList("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"));
+
+        /**
+         * Consist of ace, club, heart, diamond, respectively
+         */
         private static final List<String> POSSIBLE_SUITS = Collections.unmodifiableList(
-                Arrays.asList("\3", "\4", "\5", "\6")); // Escapes for
+                Arrays.asList("\3", "\4", "\5", "\6"));
+
         private final int value;
         private final String face;
         private final String suit;
@@ -73,13 +129,40 @@ public class chalmersw14 {
             this.suit = suit;
         }
 
+        /**
+         * Returns the face and suit of the card. Example: A♦
+         */
         public String getName() {
             return face + suit;
         }
 
+        /**
+         * Compares the face and suit of the given card to this one for sorting.
+         *
+         * @return -1 if the given card should be sorted before this card, 0 if
+         * the given card is equal to this card, or 1 if the given card should
+         * be sorted after this card.
+         */
         @Override
-        public int compareTo(Object o) {
-            return 0;
+        public int compareTo(ChalmersCard card) {
+            final int GREATER = 1;
+            final int LESSER = -1;
+            final int EQUAL = 0;
+
+            // Check for suits
+            if (card.suit.compareTo(suit) > 0) {
+                return GREATER;
+            } else if (card.suit.compareTo(suit) < 0) {
+                return LESSER;
+            }
+
+            // Check for values
+            if (card.value > value) {
+                return GREATER;
+            } else if (card.value < value) {
+                return LESSER;
+            }
+            return EQUAL;
         }
 
         @Override
